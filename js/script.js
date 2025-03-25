2144a9691baf257a1553f6c96235af906579aeba
Aryan <aryankansal15@gmail.com>
2025-03-25 20:10:09 +0530

    Delete script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const checkbox = document.getElementById('termsCheckbox');
-const submitButton = document.getElementById('submitButton');
-
-checkbox.addEventListener('change', function () {
-    submitButton.disabled = !checkbox.checked;
-});
