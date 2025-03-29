9c8c29c356061336574386e31e2cd6c3b4da6e8e
Aryan <aryankansal15@gmail.com>
2025-03-29 15:35:44 +0530

    backend project

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended : true}))
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+})
+
+app.get("/donate", (req,res) => {
+    res.render("donate.ejs");
+})
+
+app.get("/volunteer", (req,res) => {
+    res.render("volunteer.ejs");
+})
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`listening on port ${port}`);
+})
+
+
+function loginPopup(){
+    
+}
