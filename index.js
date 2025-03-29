f8979858ed17a7a2892844f2b2d7914975c514ec
Aryan <aryankansal15@gmail.com>
2025-03-29 15:44:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,27 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.use(bodyParser.urlencoded({ extended : true}))
+
+app.get("/", (req,res) => {
     res.render("index.ejs");
-});
+})
 
-app.get("/donate", (req, res) => {
+app.get("/donate", (req,res) => {
     res.render("donate.ejs");
-});
+})
 
-app.get("/volunteer", (req, res) => {
+app.get("/volunteer", (req,res) => {
     res.render("volunteer.ejs");
-});
+})
 
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-    const port = process.env.PORT || 3000;
-    app.listen(port, (err) => {
-        if (err) throw err;
-        console.log(`listening on port ${port}`);
-    });
-}
+app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`listening on port ${port}`);
+})
 
-// Remove the loginPopup function - it seems to be out of place in this server file
-
-// Export the app for Vercel
-export default app; 
+export default app;
