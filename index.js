99e10829b70bc34d405d5492073167f64b483a78
Aryan <aryankansal15@gmail.com>
2025-03-29 15:52:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
