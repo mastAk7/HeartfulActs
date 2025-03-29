7323c01d39fbc400f2fb7a7961c90e1e9e6b3937
Aryan <aryankansal15@gmail.com>
2025-03-29 15:51:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
